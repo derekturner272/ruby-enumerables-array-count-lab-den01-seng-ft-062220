9a4162a7a1d2f27d8abe0cbaed9ca24495c09a02
Derek Turner <turner.derek.b@gmail.com>
2020-06-08 18:23:27 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,7 +5,7 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  array.count do |srting|
-    srting.class == String
+  array.count do |string|
+    string.class == String.empty?
   end
 end
